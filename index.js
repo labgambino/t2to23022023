0bd3bd92cafa80ca2fa676d7554e01d1e92b3f84
labgambino <113803192+labgambino@users.noreply.github.com>
2023-02-23 11:10:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function ir (){
+    
+var c=2244; 
+var u="attec"; 
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("ATTEC: Acceso permitido al EXPEDIENTE 2019 0000013327. \nTodos los Derechos reservados."); 
+window.location="hiperenlopag.html";} 
+    
+    else { 
+        alert("Por favor, ingresa tu usuario y contraseña correctos");
+    }
+     }
